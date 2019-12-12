da8372b0ae462aeda47db83fdf5f0c7325a2da4f
Cody Toney-Griffith <cody.toney@gmail.com>
2019-12-12 20:33:01 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+speed < 60 ?  true : false
+speed < 40 ? true : false
+speed != 50 ? true : false
 end
 
 
 
 def not_safe?(speed)
-	
+speed > 60 ? true : false
+speed < 40 ? true : false
+speed != 50 ? true : false
 end
 	
-
-
