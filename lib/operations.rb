156a209a36734fd6174e89ed79c38f908a13b33e
Cody Toney-Griffith <cody.toney@gmail.com>
2019-12-12 20:20:25 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,7 @@
 def unsafe?(speed)
 speed > 60 ?  "true" : "false"
 speed < 40 ? "true" : "false"
-speed == 50 ? "false" : "true"
+speed = 50 ? "false" : "true"
 end
 
 
@@ -9,6 +9,6 @@ end
 def not_safe?(speed)
 speed > 60 ? "true" : "false"
 speed < 40 ? "true" : "false" 
-speed == 50 ? "false" : "true" 
+speed = 50 ? "false" : "true" 
 end
 	end
